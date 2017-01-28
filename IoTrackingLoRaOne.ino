b96a2cf478e372538f46763e6f0fd71778055393
Josue Alvarez <josue.alvarez@hotmail.fr>
2017-01-28 10:36:25 +0100

    Add GPS module library

--- IoTrackingLoRaOne.ino
+++ IoTrackingLoRaOne.ino
@@ -1,12 +1,11 @@
-// Accel Test
+//------------ Include libraries ------------------------ 
 
 #include <Wire.h>
 #include "Arduino.h"
 #include "Sodaq_RN2483.h"
-#include <Sodaq_UBlox_GPS.h>
-/*#include <avr/interrupt.h>
-#include <avr/power.h>
-#include <avr/sleep>*/
+#include "Sodaq_UBlox_GPS.h"
+
+//-------------- Define variables -------------------------
 
 #define CONSOLE_SERIAL SerialUSB
 #define loraSerial Serial1
@@ -28,7 +27,7 @@
 #define BAT_MAX_VALUE 65535
 #define BAT_MASK 0xF 
 
-/////////////////////////////////////////GPS, Battery & LoRa part
+//---------------- GPS, Battery & LoRa part -----------------------------
 //ABP
 const uint8_t devAddr[4] = { 0x02,0x03,0x04,0x05 };
 const uint8_t appSKey[16] = { 0x05,0xCB,0x23,0x18,0x61,0x95,0x99,0x2C,0x33,0x1D,0x29,0x8A,0x28,0x11,0xC6,0xB4 };
@@ -43,23 +42,19 @@ uint32_t intervals[] = {1UL * 60 * 10,}; size_t interval_ix = 0;
 //Coords struct
 struct rawCoords { double longitude; double latitude;}; 
 struct coords { uint16_t x; uint16_t y; };
-struct reference { double north=43.574848; double south=43.560980; double east=1.486931; double west=1.452127; }; reference fixref;
+struct reference { double north=43.573069; double south=43.567577; double east=1.472837; double west=1.462962; }; reference fixref;
 struct coords transformCoords(struct rawCoords rc) {
-    /*CONSOLE_SERIAL.println(String(rc.longitude));
+    CONSOLE_SERIAL.println(String(rc.longitude));
     CONSOLE_SERIAL.println(String(rc.latitude));
-    CONSOLE_SERIAL.println(String(fixref.west));
-    CONSOLE_SERIAL.println(String(fixref.south));
-    CONSOLE_SERIAL.println(String(fixref.east));
-    CONSOLE_SERIAL.println(String(fixref.north));
-    CONSOLE_SERIAL.println(String(rc.latitude - fixref.west));
-    CONSOLE_SERIAL.println(String(rc.longitude - fixref.south));
+    CONSOLE_SERIAL.println(String(rc.longitude - fixref.west));
+    CONSOLE_SERIAL.println(String(rc.latitude - fixref.south));
     CONSOLE_SERIAL.println(String(fixref.east - fixref.west));
     CONSOLE_SERIAL.println(String(fixref.north - fixref.south));
-    CONSOLE_SERIAL.println(String(((rc.latitude - fixref.west) / (fixref.east - fixref.west))*1023));
-    CONSOLE_SERIAL.println(String(((rc.longitude - fixref.south) / (fixref.north - fixref.south))*1023));*/
+    CONSOLE_SERIAL.println(String(((rc.longitude - fixref.west) / (fixref.east - fixref.west))*1023));
+    CONSOLE_SERIAL.println(String(((rc.latitude - fixref.south) / (fixref.north - fixref.south))*1023));
   return {
-     x: (((rc.latitude - fixref.west) / (fixref.east - fixref.west))*1023),
-     y: (((rc.longitude - fixref.south) / (fixref.north - fixref.south))*1023)
+     x: (((rc.longitude - fixref.west) / (fixref.east - fixref.west))*1023),
+     y: (((rc.latitude - fixref.south) / (fixref.north - fixref.south))*1023)
         };
 };
 
@@ -83,11 +78,13 @@ void setupLoRa(){
  //OTAA
  //setupLoRaOTAA();
 }
-///////////////////////////////////////////////////////
+//---------------- Interrupt variables ------------------
 
 volatile bool int1_flag = false;
 volatile bool int2_flag = false;
 int i = 0;
+
+//---------------- LED functions --------------------------
 
 void RED() {
   digitalWrite(LED_RED, LOW);
@@ -113,6 +110,8 @@ void OFF() {
   digitalWrite(LED_BLUE, LOW);
 }
 
+//------------------- Set up function ---------------------------
+
 void setup() 
 {
   pinMode(ENABLE_PIN_IO, OUTPUT); // ONE
@@ -128,14 +127,13 @@ void setup()
   //connect to the LoRa Network
   setupLoRa();
 
-  //CONSOLE_SERIAL.println("Testing Accelerometer");  
-  //CONSOLE_SERIAL.println("Reading from register: 0x" + String(TEST_REG, HEX));
-  //CONSOLE_SERIAL.println("Response: 0x" + String(readReg(0x0F), HEX));
-  
+  // Setup interrupts
   pinMode(ACCEL_INT1, INPUT_PULLUP);
   pinMode(ACCEL_INT2, INPUT_PULLUP);
   attachInterrupt(ACCEL_INT1, ISR1, FALLING);
   attachInterrupt(ACCEL_INT2, ISR2, FALLING);
+
+  // Setup sleep mode
   SYSCTRL->XOSC32K.bit.RUNSTDBY = 1;
   GCLK->CLKCTRL.reg = GCLK_CLKCTRL_ID(GCM_EIC) | GCLK_CLKCTRL_GEN_GCLK1 | GCLK_CLKCTRL_CLKEN;
 
@@ -179,6 +177,7 @@ void setup()
   writeReg(0x23, 0b00000000);
 #endif
 
+  // Setup LED
   pinMode(LED_RED, OUTPUT);
   pinMode(LED_GREEN, OUTPUT);
   pinMode(LED_BLUE, OUTPUT);
@@ -190,7 +189,9 @@ void setup()
   find_fix(900L* 1000);
 }
 
-/////////////////////////////////////////GPS, Battery & LoRa part
+//------------------------ GPS, Battery & LoRa part-------------------------
+
+// Function to send a LoRa packet
 void sendPacket(String packet) {
   switch (LoRaBee.send(1, (uint8_t*)packet.c_str(), packet.length()))
     {
@@ -232,17 +233,19 @@ void sendPacket(String packet) {
     }
 }
 
+// To get the battery level
 uint16_t getBatteryVoltage()
 {
     uint16_t voltage = (uint16_t)((ADC_AREF / 1.023) * (BATVOLT_R1 + BATVOLT_R2) / BATVOLT_R2 * (float)analogRead(BATVOLT_PIN));
     return voltage;
 }
 
+// To get GPS coordinates
 /*Find a GPS fix, but first wait a while */
 struct rawCoords find_fix(uint32_t delay_until)
 {  rawCoords rc_from_gps;
    uint32_t start = millis();
-   uint32_t timeout = 90L* 100;
+   uint32_t timeout = 900L* 100;
    CONSOLE_SERIAL.println(String("waiting for fix ..., timeout=") + timeout + String("ms"));
    if (sodaq_gps.scan(false, timeout)) {
         CONSOLE_SERIAL.println(String(" time to find fix: ") + (millis() - start) + String("ms"));
@@ -252,28 +255,30 @@ struct rawCoords find_fix(uint32_t delay_until)
         CONSOLE_SERIAL.println(String(" lon = ") + String(sodaq_gps.getLon(), 7));
         CONSOLE_SERIAL.println(String(" num sats = ") + String(sodaq_gps.getNumberOfSatellites()));
         //Get raw coords from gps
-        rc_from_gps= {(sodaq_gps.getLat()),(sodaq_gps.getLon())};
-        delay(10000 - delay_gps);
+        rc_from_gps= {(sodaq_gps.getLon()),(sodaq_gps.getLat())};
+        //Change here to have different rate of transmissions
+        //delay(10000 - delay_gps);
    } else {
        CONSOLE_SERIAL.println("No Fix");
    }
   return rc_from_gps;
 }
-///////////////////////////////////////////////////////
+
+//----------------------- Main Program --------------------------
 
 void loop()
 {
-  GREEN();
+  GREEN(); // Indicates that the program is running
   delay(100);
   
-  /////////////////////////////////////////GPS, Battery & LoRa part
+  //-------------- GPS, Battery & LoRa part --------------------
   //10 bits x, 10 bits y, 4 bit battery
   uint8_t data[DATA_LENGTH];
   uint32_t wait_ms = intervals[interval_ix];
   if (++interval_ix > ARRAY_DIM(intervals)) { interval_ix = 0; }
   int batlevel=100;
-  ///////////////////////////////////////////////////////
-  
+
+  // Interrupt routines
   if (int1_flag) {
     int1_flag = false;
     CONSOLE_SERIAL.println("INT1 Interrupt");
@@ -283,23 +288,22 @@ void loop()
     CONSOLE_SERIAL.println("INT2 Interrupt");
   }
 
-  SCB->SCR |= SCB_SCR_SLEEPDEEP_Msk;
+  SCB->SCR |= SCB_SCR_SLEEPDEEP_Msk; // Set sleep mode to idle
 
 
-  /////////////////////////////////////////GPS, Battery & LoRa part
+  //-------------- GPS, Battery & LoRa part ---------------------
   //Measure Battery level
   batlevel = (int(getBatteryVoltage()) / BAT_MAX_VALUE);
-
+ 
   //Get GPS coordinates
   coords c_from_transform = transformCoords(find_fix(wait_ms));
   CONSOLE_SERIAL.println(c_from_transform.x);
   CONSOLE_SERIAL.println(c_from_transform.y);
-  
   uint32_t tosend = (c_from_transform.x & COORD_MASK) | ((c_from_transform.y & COORD_MASK) << 10) | ((batlevel & BAT_MASK) << 20);
 
+  // Create and send the LoRa packet
   CONSOLE_SERIAL.println("toSend "+String(tosend, HEX));
-  
-  //C'est partie est à vérifier avec Clovis & Josué  
+   
   memcpy(data, ((uint8_t*)&tosend), DATA_LENGTH);
   
   CONSOLE_SERIAL.println("0 = "+String(data[0]));
@@ -309,13 +313,15 @@ void loop()
   String packet = (char*)data; 
   //String packet = (String)tosend;
   CONSOLE_SERIAL.println("packet = "+packet);
+  BLUE(); // Indicates the start of the transmission of a packet
   sendPacket(packet);
+  OFF(); // Indicates that the packet have been sent
+  delay(1000);
   CONSOLE_SERIAL.println("envoyé...");
-  ///////////////////////////////////////////////////////
    
-  RED();
+  RED(); // Indicates that the deivce is entering sleep mode
   delay(100);
-  __WFI();
+  __WFI(); // Go to sleep
 }
 
 uint8_t readReg(uint8_t reg)
